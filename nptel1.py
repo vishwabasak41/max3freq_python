57949de747ced1e7edb0add0e22d04e015c6fc1a
vishwabasak41 <vishwabasak95@gmail.com>
2017-08-15 23:41:26 +0530

    latest commit

--- nptel1.py
+++ nptel1.py
@@ -1,15 +1,3 @@
-def max(**dict):
-    max=0
-    for i in dict.keys():
-        if dict[i]>max:
-            max_valued_key=i;
-    return max_valued_key;
-
-def delete_key(max,**dict):
-    del dict[max]
-    return dict
-
-
 list=[]
 count_list={}
 count=0
@@ -18,24 +6,17 @@ for i in range(size):
     list.append(int(input()))
 for i in range(size):
    for j in range(size):
-       print("list[i]",list[i])
-       print("list[j]",list[j])
        if list[i]==list[j]:
            count+=1
-           print("count is",count)
-           count_list[list[i]]=count;
-           print (count_list)
-c=0
-result=[]
+           count_list[str(list[i])]=count
+   count=0
+aranged=[]
 for i in count_list.keys():
-    if c>3:
-        break
-    else:
-        maximum=max(count_list)
-        result.append(maximum)
-        count_list=delete_key(count_list,maximum)
-        print("count list after deleting max",count_list)
-        c+=1
+            newlist_sp=sorted(count_list,key=count_list.get, reverse=True)
+print(newlist_sp)
+for i in range(3):
+    aranged.append(newlist_sp[i])
+print(aranged)
 
 
 
